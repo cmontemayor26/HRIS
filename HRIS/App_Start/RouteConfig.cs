8cf7ff04378822ce9e83d6c01a5f82861d44091e
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-01-31 07:14:36 +0800

    Changed the default route to login page
    Changed the default route to login page

--- HRIS/App_Start/RouteConfig.cs
+++ HRIS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HRIS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Dashboard", action = "Dashboard", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
