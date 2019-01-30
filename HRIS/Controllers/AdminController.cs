52dd64dc8328eb1a0b38a3efe6bd65d6ecf03a43
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-01-31 07:17:16 +0800

    Dependencies
    Added some providers and packages.
    Added crypto packages

--- HRIS/Controllers/AdminController.cs
+++ HRIS/Controllers/AdminController.cs
@@ -1,14 +1,78 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HRIS.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace HRIS.Controllers
 {
     public class AdminController : Controller
     {
+        string connectionString = @"Data Source =TIM-PC; Initial Catalog = HRIS; Integrated Security=True;";
+        // Encryption
+        private string Encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        private string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+
         // GET: Admin
+        [HttpGet]
+        public ActionResult UserList()
+        {
+            DataTable listuser = new DataTable();
+            using (SqlConnection SqlCon = new SqlConnection(connectionString)) {
+                SqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM [User]", SqlCon);
+                sqlDa.Fill(listuser);
+            }
+                return View(listuser);
+        }
         public ActionResult Dashboard()
         {
             return View();
@@ -16,6 +80,25 @@ namespace HRIS.Controllers
         public ActionResult AddApplicant()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View(new UserModel());
+        }
+        [HttpPost]
+        public ActionResult AddUser(UserModel userModel)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString)) {
+                sqlCon.Open();
+                string query = "INSERT INTO [User] VALUES(@Email,@UserLevel,@Password)";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@Email", userModel.Email);
+                sqlCmd.Parameters.AddWithValue("@UserLevel", userModel.Userlevel);
+                sqlCmd.Parameters.AddWithValue("@Password", Encrypt(userModel.Password));
+                sqlCmd.ExecuteNonQuery();
+            }
+                return RedirectToAction("AddUser");
         }
     }
 }
--- HRIS/Controllers/LoginController.cs
+++ HRIS/Controllers/LoginController.cs
@@ -1,17 +1,112 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HRIS.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace HRIS.Controllers
 {
     public class LoginController : Controller
     {
+        // Encryption
+        private string Encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        private string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Login lg)
+        {
+            string mainconn = @"Data Source =TIM-PC; Initial Catalog = HRIS; Integrated Security=True;";
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = " select * from [dbo].[User] where Email = @email and Password = @password";
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlcomm.Parameters.AddWithValue("@email", lg.Email);
+            sqlcomm.Parameters.AddWithValue("@password", Encrypt(lg.Password));
+
+            SqlDataAdapter SqlDa = new SqlDataAdapter(sqlcomm);
+            DataTable Logins = new DataTable();
+            SqlDa.Fill(Logins);
+
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            if (sdr.Read())
+            {
+                for (int i = 0; i < Logins.Rows.Count; i ++) {
+                    Session["userlevel"] = Logins.Rows[i][2];
+                }
+                Session["email"] = lg.Email.ToString();
+                string userlevel = Session["userlevel"] as String;
+                if (userlevel == "Admin")
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                else if( userlevel == "Employee"){
+                    return RedirectToAction("Index", "EmployeeDashboard");
+                }
+                
+            }
+            else {
+                ViewData["Message"] = "Credentials are Invalid!";
+            }
+            sqlconn.Close();
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
