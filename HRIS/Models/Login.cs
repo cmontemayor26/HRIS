619c3a01bd57fc2dc7c49f085fcd2c16ea680cd2
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-01-31 07:06:28 +0800

    Login, User and User Model
    Login, User and User Model

---
+++ HRIS/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage ="Please enter your Email")]
+        [Display(Name ="Enter Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Password")]
+        [Display(Name = "Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string UserLevel { get; set; }
+    }
+}
---
+++ HRIS/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Models
+{
+    public class UserModel
+    {
+        public string Userid { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string Userlevel { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required.")]
+        public string Password { get; set; }
+    }
+}
