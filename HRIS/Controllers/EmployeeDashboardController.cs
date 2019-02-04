acf6bcd0dee52adb9a600be42d2fb5df72540ad7
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-02-04 12:38:55 +0800

    Employee Dashboard Controller
    Filter In/Out

--- HRIS/Controllers/EmployeeDashboardController.cs
+++ HRIS/Controllers/EmployeeDashboardController.cs
@@ -23,14 +23,39 @@ namespace HRIS.Controllers
         //   // && x.checkdate.Value >= dateFrom.Value && x.checkdate.Value <= dateTo.Value).OrderByDescending(x => x.checkdate.Value).ToList());
         //}
 
-        BIOMETRICEntities1 db = new BIOMETRICEntities1();
+        BIOMETRICEntities db = new BIOMETRICEntities();
 
-        public ActionResult Index(string empNum, DateTime? dateFrom = null, DateTime? dateTo = null)
+        public ActionResult Index(string empNum, DateTime? dateFrom, DateTime? dateTo)
         {
+            ViewBag.emp = empNum;
+            ViewBag.start = dateFrom;
+            ViewBag.end = dateTo;
 
+            var dtfrom = Convert.ToDateTime(dateFrom).Date;
+            var dtto = Convert.ToDateTime(dateTo).Date;
+
+            var item = db.qries
+                .Where(x => x.Badgenumber == empNum
+                && x.CHECKTIME >= dateFrom
+                && x.CHECKTIME.Year <= dtto.Year && x.CHECKTIME.Day <= dtto.Day && x.CHECKTIME.Month <= dtto.Month
+                )
+                //&& x.CHECKTIME <= dateTo)
+                .OrderBy(x => x.CHECKTIME)
+                .ToList();
+
+
+            return View(item);
+
+
+            //if (empNum != null)
+            //{
+
+            //}
+            //return View(db.qryCheckInOuts.Where(x => x.Badgenumber == empNum).OrderByDescending(x => x.checkdate).ToList());
             //return View(db.qryCheckInOuts.Where(x => x.Badgenumber == empNum && DateTime.Compare(x.CHECKTIME.Value.Date, dateFrom) == 0).OrderByDescending(x=>x.CHECKTIME).ToList());
-            return View(db.qryCheckInOuts1.Where(x => x.Badgenumber == empNum).OrderByDescending(x => x.checkdate).ToList());
-            //return View(db.qryCheckInOuts.Where(x => x.Badgenumber == empNum).ToList();
+
+            //return View(db.qryCheckInOuts.Where(x => x.Badgenumber == empNum).ToList());
+            //return View(db.qries.Where(x => x.Badgenumber.Contains(empNum)).ToList());
             // && x.checkdate.Value >= dateFrom.Value && x.checkdate.Value <= dateTo.Value).OrderByDescending(x => x.checkdate.Value).ToList());
         }
     }
