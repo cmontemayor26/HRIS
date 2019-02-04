96322f1c1191cb98a92a13cae0026e16dab182d1
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-02-04 16:13:39 +0800

    Updated usermodel
    Updated usermodel

--- HRIS/Models/UserModel.cs
+++ HRIS/Models/UserModel.cs
@@ -9,13 +9,16 @@ namespace HRIS.Models
 {
     public class UserModel
     {
-        public string Userid { get; set; }
+        public int Userid { get; set; }
         [Required(ErrorMessage = "This field is required.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "This field is required.")]
         public string Userlevel { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Employee Number must be numeric")]
+        public int EmployeeNumber { get; set; }
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "This field is required.")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Password length must be minimum of 8 characters")]
         public string Password { get; set; }
     }
 }
