b5d1c8cbb3a0ec82589991b58cf877ad78cf0048
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-02-11 15:57:17 +0800

    Revert "Declutter and fix filtering of attendance"
    This reverts commit bd31b776d2b32fe89daf680212cab307ebe87640.

--- HRIS/Controllers/EmployeeDashboardController.cs
+++ HRIS/Controllers/EmployeeDashboardController.cs
@@ -62,12 +62,12 @@ namespace HRIS.Controllers
             var dtfrom = Convert.ToDateTime(dateFrom).Date;
             var dtto = Convert.ToDateTime(dateTo).Date;
 
-            ViewBag.emp = employeenumber;
+            ViewBag.emp = empNum;
             ViewBag.start = dtfrom;
             ViewBag.end = dtto;
 
             var item = db.qries
-                .Where(x => x.Badgenumber == employeenumber
+                .Where(x => x.Badgenumber == empNum
                 && x.CHECKTIME >= dateFrom
                 && x.CHECKTIME.Year <= dtto.Year && x.CHECKTIME.Day <= dtto.Day && x.CHECKTIME.Month <= dtto.Month
                 )
@@ -76,27 +76,19 @@ namespace HRIS.Controllers
                 .ToList();
 
             var lateQuery = db.qries
-               .Where(x => x.Badgenumber == ((string)employeenumber)
-               && x.CHECKTYPE == "I"
-               && x.CHECKTIME.Year == DateTime.Now.Year
-               && x.CHECKTIME.Month == 1
-               && x.CHECKTIME.Hour == 7
-               && x.CHECKTIME.Minute >= 1
-               )
-               .OrderBy(x => x.CHECKTIME)
-               .Count();
+                .Where(x => x.Badgenumber == ((string)employeenumber)
+                && x.CHECKTYPE == "I"
+                && x.CHECKTIME.Year == DateTime.Now.Year
+                && x.CHECKTIME.Month == 1
+                && x.CHECKTIME.Hour == 7
+                && x.CHECKTIME.Minute >= 1
+                )
+                .OrderBy(x => x.CHECKTIME)
+                .Count();
+
             ViewBag.lateCount = lateQuery;
 
-            var firstDayOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
-            int lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1).Day;
-
-            double lateCount = Convert.ToInt32(lateQuery);
-            double daysCount = Convert.ToInt32(lastDayOfMonth);
-
-            double latePercentage = ((lateCount / daysCount) * 100);
-            ViewBag.latePercentage = latePercentage;
-
-            return View("Index",item);
+            return View(item);
         }
 
         public FileResult Reports(string ReportType)
