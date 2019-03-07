5c9afc58027b85aa0a81ef2d6ddd845027ddef51
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-03-07 16:04:00 +0800

    Essay class model
    Essay class model

---
+++ HRIS/Models/Essay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Models
+{
+
+    public class Essay
+    {
+        public string ApplicantID { get; set; }
+        public string Essay1 { get; set; }
+        public string Essay2 { get; set; }
+        public string Essay3 { get; set; }
+        public string Essay4 { get; set; }
+        public string Essay5 { get; set; }
+        public string Essay6 { get; set; }
+        public string Essay7 { get; set; }
+        public string Essay8 { get; set; }
+        public string Essay9 { get; set; }
+        public string Essay10 { get; set; }
+        public string Essay11 { get; set; }
+        public string Essay12 { get; set; }
+        public string Essay13 { get; set; }
+    }
+}
