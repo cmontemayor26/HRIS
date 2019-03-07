604faea3384495711de06ab7da946c83c8777e22
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-03-07 16:04:10 +0800

    IQtest class model
    IQtest class model

---
+++ HRIS/Models/IQtest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Models
+{
+    public class IQtest
+    {
+        public int ApplicantID { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest1 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest2 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest3 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest4 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest5 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest6 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest7 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest8 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest9 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest10 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest11 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest12 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest13 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest14 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest15 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest16 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest17 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest18 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest19 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest20 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest21 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest22 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest23 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest24 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest25 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest26 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest27 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest28 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest29 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string IQtest30 { get; set; }
+    }
+}
