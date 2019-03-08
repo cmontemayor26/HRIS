bf1541b604baf12f017a412b91f4ffc1ebfd4442
CarloMontemayor <39406014+CarloMontemayor@users.noreply.github.com>
2019-03-08 16:09:57 +0800

    Required validation
    Required validation

--- HRIS/Models/Essay.cs
+++ HRIS/Models/Essay.cs
@@ -11,18 +11,31 @@ namespace HRIS.Models
     public class Essay
     {
         public string ApplicantID { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay1 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay2 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay3 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay4 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay5 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay6 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay7 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay8 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay9 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay10 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay11 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay12 { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Essay13 { get; set; }
     }
 }
